bb387852947fe3566f8eca8e21d31a6fcdeac497
xiongchenyan <xiongchenyan@gmail.com>
2016-11-03 15:08:02 -0400

    update

--- knowledge4ir/prepare/bow/ClueWeb09/align_msra_parsed_doc.py
+++ knowledge4ir/prepare/bow/ClueWeb09/align_msra_parsed_doc.py
@@ -29,10 +29,15 @@ def align_doc_url(doc_text_in, doc_url_in, out_name):
     out = open(out_name, "w")
     cnt = 0
     for line in open(doc_text_in):
-        url, text = line.strip().split('\t')
+        line = line.strip()
+        cols = line.split('\t')
+        if len(cols) != 2:
+            logging.warning(line)
+
+        url, text = '\t'.join(cols[:-1]), cols[-1]
         if url in h_url_no:
             docno = h_url_no[url]
-            print >> out, docno + "\t" + line.strip()
+            print >> out, docno + "\t" + line
             cnt += 1
     out.close()
     logging.info("finished [%s][%s] with [%d] found", doc_text_in, doc_url_in, cnt)
---
+++ knowledge4ir/prepare/bow/ClueWeb09/fetch_doc_text.py
@@ -0,0 +1,16 @@
+"""
+fetch the texts for docno's in trec ranking file
+    for now there is no fields
+input:
+    trec ranking
+    doc_text directory
+        each file is a partition parsed text
+            docno \t url \t text
+output:
+    doc info file
+        docno \t h{'docno:', 'field(now it is bodyText':text}
+
+"""
+
+
+
