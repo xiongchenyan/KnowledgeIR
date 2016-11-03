c8cc3aa68fbef1826e75bfdaaad045983fac1407
xiongchenyan <xiongchenyan@gmail.com>
2016-11-03 14:54:12 -0400

    logging

--- knowledge4ir/prepare/bow/ClueWeb09/align_msra_parsed_doc.py
+++ knowledge4ir/prepare/bow/ClueWeb09/align_msra_parsed_doc.py
@@ -8,18 +8,26 @@ output:
 """
 
 import sys
+import logging
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 
 
 def align_doc_url(doc_text_in, doc_url_in, out_name):
     s_url_no = set([line.split('\t')[0] for line in open(doc_text_in)])
+    logging.info('%d doc text total', len(s_url_no))
     h_url_no = {}
+    err_cnt = 0
     for line in open(doc_url_in):
-        url, docno = line.strip().split('\t')
+        cols = line.strip().split('\t')
+        if len(cols) != 2:
+            logging.warning('url format error [%s]', line)
+            err_cnt += 1
+            continue
+        url, docno = cols[0], cols[-1]
         if url in s_url_no:
             h_url_no[url] = docno
-
+    logging.info('%d doc url in this partition %d err url', len(h_url_no), err_cnt)
     out = open(out_name, "w")
     cnt = 0
     for line in open(doc_text_in):
@@ -29,10 +37,12 @@ def align_doc_url(doc_text_in, doc_url_in, out_name):
             print >> out, docno + "\t" + line.strip()
             cnt += 1
     out.close()
-    print "finished [%s][%s] with [%d] found" %(doc_text_in, doc_url_in, cnt)
+    logging.info("finished [%s][%s] with [%d] found", doc_text_in, doc_url_in, cnt)
 
 
 if __name__ == '__main__':
+    from knowledge4ir.utils import set_basic_log
+    set_basic_log()
     if len(sys.argv) != 4:
         print "3 para: doc text in, doc url in, out_name"
         sys.exit()
