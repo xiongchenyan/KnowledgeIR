f6658673135949145c004182de140ae28239d4eb
xiongchenyan <xiongchenyan@gmail.com>
2016-11-04 15:28:54 -0400

    init qsub for cv

---
+++ knowledge4ir/cv/run_mul_kfolds.py
@@ -0,0 +1,11 @@
+"""
+submit multiple kfold cv for combination of feature files and model files
+input:
+    list of feature names
+    list of model configs
+    feature dir
+    model dir
+    out dir
+do:
+    submit job for each feature-model conf pair
+"""
