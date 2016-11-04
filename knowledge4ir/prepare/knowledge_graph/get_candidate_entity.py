d527ba275ca78de49dbdfc3425803d5bb8d6210b
xiongchenyan <xiongchenyan@gmail.com>
2016-11-04 15:12:03 -0400

    update

--- knowledge4ir/prepare/knowledge_graph/get_candidate_entity.py
+++ knowledge4ir/prepare/knowledge_graph/get_candidate_entity.py
@@ -26,6 +26,7 @@ class GetCandidateEntity(Configurable):
 
     def _get_per_f(self, fname):
         l_e = []
+        logging.info('started [%s]', fname)
         for line in open(fname):
             h = json.loads(line.split('\t')[-1])
             for linker in self.l_linker:
@@ -33,17 +34,20 @@ class GetCandidateEntity(Configurable):
                     continue
                 for field in self.l_target_fields:
                     if field in h:
-                        l_e.extend([ana[0] for ana in h[field]])
-        logging.info('[%d] candidate get from [%s]'. len(l_e), fname)
+                        l_e.extend(list(set([ana[0] for ana in h[field]])))
+        logging.info('[%d] candidate get from [%s]', len(l_e), fname)
         return l_e
 
     def get_candidate_e(self):
-        s_e = set()
+        l_total = []
         for fname in self.l_target_fname:
             l_e = self._get_per_f(fname)
-            s_e.update(set(l_e))
-        print >> open(self.out_name,'w'), '\n'.join(list(s_e))
-        logging.info('total [%d] candidate entities', len(s_e))
+            l_total.extend(list(set(l_e)))
+        l_total = list(set(l_total))
+        out = open(self.out_name, 'w')
+        for e in l_total:
+            print >> out, e
+        logging.info('total [%d] candidate entities', len(l_total))
         return
 
 
