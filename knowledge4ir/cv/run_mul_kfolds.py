f441b8502059be30ff957bc67e6ba3bca9d882c4
xiongchenyan <xiongchenyan@gmail.com>
2016-11-07 11:22:12 -0500

    multiple submit of kfold

--- knowledge4ir/cv/run_mul_kfolds.py
+++ knowledge4ir/cv/run_mul_kfolds.py
@@ -11,3 +11,54 @@ do:
 """
 
 
+import logging
+import json
+import sys
+import os
+from traitlets.config import Configurable
+from traitlets import (
+    Unicode,
+    List,
+    Int,
+)
+from knowledge4ir.utils.condor import qsub_job
+
+
+class RunMulKFold(Configurable):
+    l_feature_names = List(Unicode, help='list of feature files').tag(config=True)
+    l_model_configs = List(Unicode, help='list of model configs').tag(config=True)
+    feature_dir = Unicode(help='the dir of features').tag(config=True)
+    model_dir = Unicode('.', help='the dir of configs').tag(config=True)
+    out_dir = Unicode(help='the output root directory').tag(config=True)
+
+    def submit(self):
+        for feature_name in self.l_feature_names:
+            for model_config in self.l_model_configs:
+                out_name = feature_name + '.' + model_config
+                l_cmd = ['python', 'kfold_pipe.py',
+                         os.path.join(self.feature_dir, feature_name),
+                         os.path.join(self.model_dir, model_config),
+                         os.path.join(self.out_dir, out_name)
+                         ]
+                qsub_job(l_cmd)
+        logging.info('all submited')
+        return
+
+
+if __name__ == '__main__':
+    from knowledge4ir.utils import set_basic_log, load_py_config
+    set_basic_log()
+    if 2 != len(sys.argv):
+        print "multiple submit kfold jobs"
+        print '1 para: config'
+        RunMulKFold.class_print_help()
+        sys.exit(-1)
+    conf = load_py_config(sys.argv[1])
+    runner = RunMulKFold(config=conf)
+    runner.submit()
+
+
+
+
+
+
