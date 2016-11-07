ba4ce835dd9523bcad0151e9d977cb62f673f307
xiongchenyan <xiongchenyan@gmail.com>
2016-11-07 10:37:46 -0500

    debug

--- knowledge4ir/utils/base_conf.py
+++ knowledge4ir/utils/base_conf.py
@@ -8,7 +8,7 @@ this is sth to be used for impoet
 from os import path
 ROOT_PATH = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 GDEVAL_PATH = ROOT_PATH + '/knowledge4ir/utils/gdeval.pl'
-RANKSVM_PATH = ROOT_PATH + '/knowledge4ir/model/rank_svm'
+RANKSVM_PATH = ROOT_PATH + '/knowledge4ir/model/svm_rank'
 RANKLIB_PATH = ROOT_PATH + '/knowledge4ir/model/RankLib.jar'
 TMP_DIR = ROOT_PATH + '/tmp/S2Ranking/'
 DATA_PATH = "/bos/usr0/cx/tmp/knowledge4ir/data/"
