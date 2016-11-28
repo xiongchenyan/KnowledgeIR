4c9a53c27a8aeab1a302590466ced5c6cdbfedf6
xiongchenyan <xiongchenyan@gmail.com>
2016-11-28 14:10:19 -0500

    implement top k most similar textual similarity in doc'e vs q

--- knowledge4ir/feature/q_de_text.py
+++ knowledge4ir/feature/q_de_text.py
@@ -5,9 +5,8 @@ features from query itself, to document's bag-of-entities
 a subclass of LeToRFeatureExtractor
 
 features:
-    tf weighted q-doc e textual similarities
-    coverage of doc e's name on query
-    e's rank in query's reference entity ranking (indri, FACC1)
+    q-doc e textual similarities
+        bins, and top k
 
 
 """
@@ -34,38 +33,30 @@ import logging
 import json
 
 
-class LeToRDocEntityFeatureExtractorC(LeToRFeatureExtractor):
+class LeToRQDocETextFeatureExtractorC(LeToRFeatureExtractor):
     feature_name_pre = Unicode('ERank')
     l_text_fields = List(Unicode, default_value=TARGET_TEXT_FIELDS).tag(config=True)
     l_model = List(Unicode,
-                   default_value=['lm_dir', 'bm25', 'coordinate', 'tf_idf']
+                   default_value=['lm_dir', 'coordinate', 'tf_idf']
                    ).tag(config=True)
     l_pooling = List(Unicode,
-                     default_value=['tf', 'max']).tag(config=True)
-    l_rank_feature = List(Unicode,
-                          default_value=['err']
-                          ).tag(config=True)
-    l_entity_fields = List(Unicode, default_value=['name', 'alias', 'desp'])
+                     default_value=['topk']).tag(config=True)
+    top_k = Int(5, help='top 5 most similar entities to count').tag(config=True)
+
+    l_entity_fields = List(Unicode, default_value=['desp'])
     entity_text_in = Unicode(help="entity texts in").tag(config=True)
     tagger = Unicode('tagme', help='tagger used, as in q info and d info'
                      ).tag(config=True)
     corpus_stat_pre = Unicode(help="the file pre of corpus stats").tag(config=True)
-    l_ref_rank = List(Unicode, help='query reference entity ranking').tag(config=True)
-    l_ref_rank_name = List(Unicode, help='query reference rank name').tag(config=True)
-    l_top_k = List(Int, default_value=[1, 5, 10, 20, 100],
-                   help='ref rank overlap top k to consider'
-                   ).tag(config=True)
 
     def __init__(self, **kwargs):
-        super(LeToRDocEntityFeatureExtractorC, self).__init__(**kwargs)
+        super(LeToRQDocETextFeatureExtractorC, self).__init__(**kwargs)
         self.h_corpus_stat = {}
         self.h_field_df = {}
         self._load_corpus_stat()
         self.h_entity_texts = {}
         if self.entity_text_in:
             self.h_entity_texts = load_entity_texts(self.entity_text_in)
-        self.l_h_q_ref_ranking = [dict(load_trec_ranking_with_score(ranking_in))
-                                  for ranking_in in self.l_ref_rank]
         self.s_model = set(self.l_model)
 
     def _load_corpus_stat(self):
@@ -87,9 +78,6 @@ class LeToRDocEntityFeatureExtractorC(LeToRFeatureExtractor):
         h_doc_e_texts = self._prepare_doc_e_texts(l_e)
 
         h_feature.update(self._extract_q_doc_e_textual_features(query, l_h_doc_e_lm, h_doc_e_texts))
-
-        h_feature.update(self._extract_q_doc_e_ref_rank_feature(qid, l_h_doc_e_lm))
-
         return h_feature
 
     def _form_doc_e_lm(self, h_doc_info):
@@ -158,6 +146,8 @@ class LeToRDocEntityFeatureExtractorC(LeToRFeatureExtractor):
             h_pooled_scores.update(self._max_pool_entity_sim(l_h_scores))
         if 'tf' in self.l_pooling:
             h_pooled_scores.update(self._wsum_pool_entity_sim(l_h_scores, l_e_tf))
+        if 'topk' in self.l_pooling:
+            h_pooled_scores.update(self._topk_pool_entity_sim(l_h_scores))
         return h_pooled_scores
 
     @classmethod
@@ -179,51 +169,21 @@ class LeToRDocEntityFeatureExtractorC(LeToRFeatureExtractor):
                 h_wsum['Wsum' + key] = score * w + h_wsum.get(key, 0)
         return h_wsum
 
-    def _extract_q_doc_e_ref_rank_feature(self, qid, l_h_doc_e_lm):
-        """
-        check how many of the entities is in top 1, 10
-        :param qid:
-        :param l_h_doc_e_lm:
-        :return:
-        """
-        h_feature = {}
-        l_q_rank = [h_q_rank.get(qid, []) for h_q_rank in self.l_h_q_ref_ranking]
-        l_q_ref_rank_p = []
-        for rank in l_q_rank:
-            h = dict(zip([doc for doc, __ in rank], range(1, len(l_q_rank) + 1)))
-            l_q_ref_rank_p.append(h)
+    def _topk_pool_entity_sim(self, l_h_scores):
+        h_topk = {}
+        h_key_scores = {}
+        for h_scores in l_h_scores:
+            for key, score in h_scores.items():
+                if key not in h_key_scores:
+                    h_key_scores[key] = [score]
+                else:
+                    h_key_scores[key].append(score)
 
-        for field, h_doc_e_lm in zip(self.l_text_fields, l_h_doc_e_lm):
-            if field == 'bodyText':
-                for ref_name, h_ref_rank_p in zip(self.l_ref_rank_name, l_q_ref_rank_p):
-                    l_e_rank_p = []
-                    for e in h_doc_e_lm.keys():
-                        p = h_ref_rank_p.get(e, 10000000)
-                        l_e_rank_p.append(p)
-                    l_top_k_cnt = self._count_topk(l_e_rank_p, self.l_top_k)
-                    for top_k, top_k_cnt in zip(self.l_top_k, l_top_k_cnt):
-                        feature_name = self.feature_name_pre + ref_name.title()
-                        h_feature[feature_name + 'Top%03d' % top_k] = top_k_cnt
-        return h_feature
-
-
-    @classmethod
-    def _count_topk(cls, l_ranks, l_top_k):
-        l_top_k_cnt = []
-        l_p = sorted(l_ranks)
-        i = 0
-        l_top_k_cnt.append(0)
-        for p in l_p:
-            if i >= len(l_top_k):
-                break
-            while p > l_top_k[i]:
-                i += 1
-                if i >= len(l_top_k):
-                    break
-                l_top_k_cnt.append(l_top_k_cnt[-1])
-
-            l_top_k_cnt[-1] += 1
-        while len(l_top_k_cnt) < len(l_top_k):
-            l_top_k_cnt.append(l_top_k_cnt[-1])
-        return l_top_k_cnt
+        for key, l_score in h_key_scores.items():
+            l_score.sort(reverse=True)
+            while len(l_score) < self.top_k:
+                l_score.append(-20)
+            for k in xrange(self.top_k):
+                h_topk[key + 'Top%d' % (k + 1)] = l_score[k]
+        return h_topk
 
