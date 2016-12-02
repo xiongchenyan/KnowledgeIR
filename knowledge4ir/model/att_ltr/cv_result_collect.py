a26e9e0e3e96d4d585492b78176e1cfc601ac9ba
xiongchenyan <xiongchenyan@gmail.com>
2016-12-02 15:35:24 -0500

    update cv result collect

--- knowledge4ir/model/att_ltr/cv_result_collect.py
+++ knowledge4ir/model/att_ltr/cv_result_collect.py
@@ -5,14 +5,13 @@ collect cv results
 import sys
 import subprocess
 import os
-from scholarranking.utils import (
+from knowledge4ir.utils import (
     GDEVAL_PATH,
-    QREL_PATH
 )
 
-if 2 != len(sys.argv):
+if 3 != len(sys.argv):
     print "collect cv results"
-    print '1 para: cv dir'
+    print '2 para: cv dir + qrel'
     sys.exit()
 
 l_rank_lines = []
@@ -28,7 +27,7 @@ print >> open(rank_out_name, 'w'), '\n'.join(l_rank_lines).strip()
 
 for d in [1, 3, 5, 10, 20]:
     eva_out = subprocess.check_output([
-        'perl', GDEVAL_PATH, '-k', '%d' %d, QREL_PATH, rank_out_name])
+        'perl', GDEVAL_PATH, '-k', '%d' %d, sys.argv[2], rank_out_name])
     print >> open(os.path.join(sys.argv[1], 'eval.d%02d' % d), 'w'), eva_out.strip()
     print "d %d: %s" % (d, eva_out.splitlines()[-1])
 
