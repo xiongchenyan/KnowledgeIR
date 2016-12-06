a153b7bd7b52d267a07fc8fcafc0d66b468e0915
xiongchenyan <xiongchenyan@gmail.com>
2016-12-06 17:29:54 -0500

    default

--- knowledge4ir/model/att_ltr/cv_result_collect.py
+++ knowledge4ir/model/att_ltr/cv_result_collect.py
@@ -9,9 +9,9 @@ from knowledge4ir.utils import (
     GDEVAL_PATH,
 )
 
-if 3 != len(sys.argv):
+if 2 > len(sys.argv):
     print "collect cv results"
-    print '2 para: cv dir + qrel'
+    print '1 para: cv dir + qrel (opt, default /bos/usr0/cx/tmp/data/qrel.all)'
     sys.exit()
 
 l_rank_lines = []
@@ -25,9 +25,13 @@ for dir_name, sub_dirs, file_names in os.walk(sys.argv[1]):
 rank_out_name = os.path.join(sys.argv[1], 'trec')
 print >> open(rank_out_name, 'w'), '\n'.join(l_rank_lines).strip()
 
+
+qrel_in = '/bos/usr0/cx/tmp/data/qrel.all'
+if len(sys.argv) > 2:
+    qrel_in = sys.argv[2]
 for d in [1, 3, 5, 10, 20]:
     eva_out = subprocess.check_output([
-        'perl', GDEVAL_PATH, '-k', '%d' %d, sys.argv[2], rank_out_name])
+        'perl', GDEVAL_PATH, '-k', '%d' %d, qrel_in, rank_out_name])
     print >> open(os.path.join(sys.argv[1], 'eval.d%02d' % d), 'w'), eva_out.strip()
     print "d %d: %s" % (d, eva_out.splitlines()[-1])
 
