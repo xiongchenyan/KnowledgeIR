007590325baa226e8fec0cc2c22a40a91c75ccd7
xiongchenyan <xiongchenyan@gmail.com>
2016-12-08 13:36:54 -0500

    debug

--- knowledge4ir/prepare/knowledge_graph/local_term_entity_embedding_data.py
+++ knowledge4ir/prepare/knowledge_graph/local_term_entity_embedding_data.py
@@ -1,14 +1,13 @@
 """
 generate locally term and entity embedding from PRF docs
 input:
-    tagme's doc info
+    tagme's linked results
 output:
     for each doc, two lines:
         body text
         body text with entity replaced
 """
 
-import json
 import sys
 
 
@@ -20,12 +19,19 @@ if 3 != len(sys.argv):
 out = open(sys.argv[2], 'w')
 
 for cnt, line in enumerate(open(sys.argv[1])):
-    d_info = json.loads(line.split('\t')[-1])
-    text = d_info['bodyText']
+    d_info = '\t'.join(line.strip().split('\t')[1:])
+    text = d_info.split('#')[0]
+    ana_str = d_info.split('#')[-1]
+    l_col = ana_str.split('\t')
+    l_ana = []
+    p = 0
+    while p + 8 <= len(l_col):
+        ana = [l_col[p + 6], l_col[p + 2], l_col[p + 3]]
+        p += 8
     print >> out, text
     e_text = ""
     p = 0
-    for ana in d_info['tagme']['bodyText']:
+    for ana in l_ana:
         st, ed = ana[1], ana[2]
         e_text += text[p:st] + ana[0]
         p = ed
