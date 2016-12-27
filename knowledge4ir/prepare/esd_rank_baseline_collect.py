b0e6ced7163e37e097ec3da258182a7578dc46e7
xiongchenyan <xiongchenyan@gmail.com>
2016-12-27 15:13:41 -0500

    tyope

--- knowledge4ir/prepare/esd_rank_baseline_collect.py
+++ knowledge4ir/prepare/esd_rank_baseline_collect.py
@@ -20,7 +20,7 @@ out = open(sys.argv[2], 'w')
 for qid in xrange(1, 251):
     l_docno = open(os.path.join(sys.argv[1], '%d_doc_docNo' % qid)).read().splitlines()
     l_rank = open(os.path.join(sys.argv[1], '%d_prediction' % qid)).read().splitlines()
-
+    l_rank = [int(r) for r in l_rank]
     l_doc_rank = zip(l_docno, l_rank)
     l_doc_rank.sort(key=lambda item: item[1])
     for doc, rank in l_doc_rank:
