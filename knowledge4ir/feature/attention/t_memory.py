cc75c2abe3872387210c5fdd16afb6f4636b330e
xiongchenyan <xiongchenyan@gmail.com>
2017-01-03 12:52:04 -0500

    quick implementation

--- knowledge4ir/feature/attention/t_memory.py
+++ knowledge4ir/feature/attention/t_memory.py
@@ -2,3 +2,23 @@
 memory based features
     word name
 """
+
+from knowledge4ir.feature.attention import (
+    TermAttentionFeature,
+)
+from traitlets import (
+    Unicode,
+)
+
+
+class TermMemoryAttentionFeature(TermAttentionFeature):
+    feature_name_pre = Unicode("TermMem")
+
+    def extract(self, h_q_info, l_w):
+        l_h_feature = []
+        for w in l_w:
+            h = dict()
+            h[self.feature_name_pre + w.lower()] = 1
+            l_h_feature.append(h)
+        return l_h_feature
+
