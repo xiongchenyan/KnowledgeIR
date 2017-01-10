26bf5260d5ba79200eb2c10ee6263da6009fbfd1
xiongchenyan <xiongchenyan@gmail.com>
2017-01-10 16:50:22 -0500

    init

---
+++ knowledge4ir/analysis/rank_component_analysis.py
@@ -0,0 +1,31 @@
+"""
+analysis ranking component of qw_de qe_de
+input:
+    q info
+    trec rank with doc info
+output:
+    for each q-d pair:
+        top k entity search score's entity
+        for each query entity, doc's entity in each transE bin
+"""
+
+from traitlets.config import Configurable
+from traitlets import (
+    Unicode,
+    List,
+)
+from knowledge4ir.feature import LeToRFeatureExternalInfo
+from knowledge4ir.utils import (
+    load_query_info,
+    load_trec_ranking_with_info,
+)
+import numpy as np
+import json
+import logging
+
+
+class RankComponentAna(Configurable):
+    
+    def __init__(self, **kwargs):
+        super(RankComponentAna, self).__init__(**kwargs)
+        
