326e8876a6fca89dcdbd588216fea2b18aa507ed
xiongchenyan <xiongchenyan@gmail.com>
2017-03-10 11:50:40 -0500

    add sf dict filter

---
+++ knowledge4ir/joint/scripts/surfaceform_dict_filter.py
@@ -0,0 +1,33 @@
+"""
+only keep those appear in the data, to speed up
+input:
+    spotted q or doc (or both)
+    sf dict
+output:
+    sf dict with those only appear in the spot
+"""
+
+import json
+import sys
+
+if 4 >= len(sys.argv):
+    print "3+ para: sf dict + out name + all spotted files"
+    sys.exit(-1)
+
+h_target = {}
+h_sf_dict = json.load(open(sys.argv[1]))
+
+for in_name in sys.argv[3:]:
+    h_info = json.loads(in_name)
+    h_field_ana = h_info.get('spot', {})
+    for field, l_ana in h_field_ana.items():
+        for ana in l_ana:
+            sf = ana.get('surface', '')
+            if sf in h_sf_dict:
+                if sf not in h_target:
+                    h_target[sf] = h_sf_dict[sf]
+
+print "total [%d] sf to keep" % (len(h_target))
+json.dump(h_target, open(sys.argv[2], 'w'), indent=1)
+print "dumped"
+
