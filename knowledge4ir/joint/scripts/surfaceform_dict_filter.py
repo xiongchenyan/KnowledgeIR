3293823382a9c9ec52f41e3984fad05ac209e55f
xiongchenyan <xiongchenyan@gmail.com>
2017-03-10 11:55:59 -0500

    debug

--- knowledge4ir/joint/scripts/surfaceform_dict_filter.py
+++ knowledge4ir/joint/scripts/surfaceform_dict_filter.py
@@ -18,14 +18,15 @@ h_target = {}
 h_sf_dict = json.load(open(sys.argv[1]))
 
 for in_name in sys.argv[3:]:
-    h_info = json.loads(in_name)
-    h_field_ana = h_info.get('spot', {})
-    for field, l_ana in h_field_ana.items():
-        for ana in l_ana:
-            sf = ana.get('surface', '')
-            if sf in h_sf_dict:
-                if sf not in h_target:
-                    h_target[sf] = h_sf_dict[sf]
+    for line in open(in_name):
+        h_info = json.loads(line)
+        h_field_ana = h_info.get('spot', {})
+        for field, l_ana in h_field_ana.items():
+            for ana in l_ana:
+                sf = ana.get('surface', '')
+                if sf in h_sf_dict:
+                    if sf not in h_target:
+                        h_target[sf] = h_sf_dict[sf]
 
 print "total [%d] sf to keep" % (len(h_target))
 json.dump(h_target, open(sys.argv[2], 'w'), indent=1)
