f146d0986948afc64851184d0ee4ec072eae9ee2
xiongchenyan <xiongchenyan@gmail.com>
2017-03-23 15:51:14 -0400

    init overfit analysis

---
+++ knowledge4ir/joint/grounding/mem_e.py
@@ -0,0 +1,21 @@
+"""
+overfit entities
+
+sf feature only a bias
+entity feature is the entity id
+
+will only be used to overfit the results
+
+serve as a subclass of base.Grounder
+only need to replace the extract_for_surface and extract_for_entity
+
+"""
+
+from knowledge4ir.joint.grounding import Grounder
+import json
+
+
+class OverfitGrounder()
+
+
+
