9cc343db4bfd280f2c4a89f3cf5939008c555b5c
xiongchenyan <xiongchenyan@gmail.com>
2017-03-24 16:38:46 -0400

    wrong qid format

--- knowledge4ir/model/overfit.py
+++ knowledge4ir/model/overfit.py
@@ -23,7 +23,7 @@ conf = load_py_config(sys.argv[1])
 in_name = sys.argv[2]
 out_dir = sys.argv[3]
 cv = CrossValidator(config=conf)
-s_qid = set(range(cv.q_range[0], cv.q_range[1] + 1))
+s_qid = set(['%d' % qid for qid in range(cv.q_range[0], cv.q_range[1] + 1)])
 cv.train_test_generator(in_name, in_name, out_dir, s_qid, s_qid)
 
 
