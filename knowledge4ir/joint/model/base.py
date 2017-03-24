3e1de5df312674891e150e1c56aff549166d734a
xiongchenyan <xiongchenyan@gmail.com>
2017-03-24 14:12:48 -0400

    api change

--- knowledge4ir/joint/model/base.py
+++ knowledge4ir/joint/model/base.py
@@ -149,7 +149,7 @@ class JointSemanticModel(ModelBase):
         steps = len(s_target_qid)
         self.training_model.fit_generator(
             self.pairwise_data_generator(in_name, s_target_qid),
-            steps_per_epoch=steps,
+            samples_per_epoch=steps,
             nb_epoch=hyper_para.nb_epoch,
             callbacks=[EarlyStopping(monitor='loss',
                                      patience=hyper_para.early_stopping_patient
