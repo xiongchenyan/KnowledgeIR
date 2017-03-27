01476dc02770ca399ae93c1df0bb55856eca60da
xiongchenyan <xiongchenyan@gmail.com>
2017-03-27 13:02:12 -0400

    debug

--- knowledge4ir/joint/analysis/pretty_comp_e_att.py
+++ knowledge4ir/joint/analysis/pretty_comp_e_att.py
@@ -56,6 +56,8 @@ class PrettyCompEAtt(Configurable):
             sf = ' '.join(l_qt[loc[0]: loc[1]])
             for j in xrange(len(l_info[0]['e_ref'])):
                 e_id, root_score, name = l_info[0]['e_att_score'][i][j]
+                if type(root_score) == list:
+                    root_score = root_score[0]
                 this_line = q_pre + '\t' + '\t'.join([sf, e_id, name]) + '\t%f' % root_score
                 for info in l_info[1:]:
                     e_score = info['e_att_score'][i][j][1]
