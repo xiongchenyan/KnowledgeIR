5d7b29ea2ed00a4a1b206bb172bd8ae7fe3c48e7
hunterhector <hunterhector@gmail.com>
2017-04-05 17:17:50 -0400

    Adding methods to write out wiki text with free base id replaced.

---
+++ linker/data/data_utils.py
@@ -0,0 +1,17 @@
+import os, logging, pickle
+
+
+def run_or_load(path, func, *args):
+    if os.path.exists(path):
+        logging.info("Loading data from %s." % path)
+        return pickle.load(open(path))
+    else:
+        logging.info("No saved data found.")
+        result = func(*args)
+        logging.info("Done processing, pickling as middle results.")
+
+        with open(path, 'w') as link_pickle_f:
+            pickle.dump(result, link_pickle_f)
+        logging.info("Done pickling.")
+
+        return result
