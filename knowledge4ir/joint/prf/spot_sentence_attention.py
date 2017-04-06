1da0682fe10c444d8ba607a0837974ca9746a91b
xiongchenyan <xiongchenyan@gmail.com>
2017-04-06 14:39:50 -0400

    debug

--- knowledge4ir/joint/prf/spot_sentence_attention.py
+++ knowledge4ir/joint/prf/spot_sentence_attention.py
@@ -74,6 +74,7 @@ class SpotSentAttention(Configurable):
             l_qid.append(qid)
             l_sentno.append(sentno)
             l_score.append(score)
+            l_sent.append(sent)
 
         dump_trec_out_from_ranking_score(l_qid, l_sentno, l_score, self.out_name, 'emb_cos', l_sent)
         logging.info('finished')
