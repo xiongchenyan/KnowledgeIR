68b1a2f4891752ecc449116f5021656f8b826e26
hunterhector <hunterhector@gmail.com>
2017-04-25 23:30:40 -0400

    Read the wiki ids from the dbpedia dump instead of wikidata.

--- linker/data/freebase_wiki_mapper.py
+++ linker/data/freebase_wiki_mapper.py
@@ -9,6 +9,7 @@ from wiki_sql_linker import WbItemsPerSite
 
 freebase_prefix = "http://rdf.freebase.com/ns/"
 wikidata_prefix = "http://www.wikidata.org/entity/"
+dbpeida_prefix = "http://dbpedia.org/resource/"
 
 
 class FreebaseWikiMapper:
@@ -19,7 +20,26 @@ class FreebaseWikiMapper:
         if not os.path.exists(mapper_dir):
             os.makedirs(mapper_dir)
 
-    def create_mapping(self, fb_wd_mapping_path, wb_database_name, db_user_id, db_passwd):
+    def create_mapping_dbpedia(self, fb_wiki_mapping_path):
+        mapping_file = os.path.join(self.mapper_dir, self.fb_wiki_mapping_file)
+
+        if os.path.exists(mapping_file):
+            print("Mapping file exists, not overwriting")
+            return
+
+        with open(mapping_file, 'w') as out:
+            count = 0
+            for statements in NIFParser(fb_wiki_mapping_path):
+                for s, v, o in statements:
+                    if str(v) == 'http://www.w3.org/2002/07/owl#sameAs':
+                        fb_id = data_utils.canonical_freebase_id((str(o).replace(freebase_prefix, "")))
+                        wikipage_name = s.encode("utf-8").replace(dbpeida_prefix, "")
+                        out.write("%s\t%s\n" % (fb_id, wikipage_name))
+                        count += 1
+                        sys.stdout.write("\r[%s] found %d pairs." % (datetime.datetime.now().time(), count))
+            print("\nTotally %s mappings created." % count)
+
+    def create_mapping_wiki_data_sql(self, fb_wd_mapping_path, wb_database_name, db_user_id, db_passwd):
         wb_db = WbItemsPerSite(db_user_id, db_passwd, wb_database_name)
 
         mapping_file = os.path.join(self.mapper_dir, self.fb_wiki_mapping_file)
@@ -45,20 +65,14 @@ class FreebaseWikiMapper:
                         else:
                             missed += 1
 
-            print("Totally %s mappings created, %d freebase items are not mapped." % (count, missed))
+            print("\nTotally %s mappings created, %d freebase items are not mapped." % (count, missed))
 
     def read_wiki_fb_mapping(self):
         wiki_2_fb = {}
         with open(os.path.join(self.mapper_dir, self.fb_wiki_mapping_file)) as mapping:
             for line in mapping:
-                fb_id, wikipage_name, wd_id = line.strip().split("\t")
-
-                # The data read directly from the MySQL database use spaces.
+                fb_id, wikipage_name = line.strip().split("\t")[0:2]
                 formatted_wiki_name = wikipage_name.replace(" ", "_")
-
-                # if wd_id == "Q3217492" or wd_id == "Q6156388":
-                #     print formatted_wiki_name, wd_id, formatted_wiki_name.__class__
-
                 wiki_2_fb[formatted_wiki_name] = fb_id
         return wiki_2_fb
 
@@ -69,7 +83,7 @@ def main():
     mapper_dir = sys.argv[2]
 
     mapper = FreebaseWikiMapper(mapper_dir)
-    mapper.create_mapping(fb_wd_mapping, "wikidatawiki_wb_items_per_site", "hector", "hector")
+    mapper.create_mapping_wiki_data_sql(fb_wd_mapping, "wikidatawiki_wb_items_per_site", "hector", "hector")
     wiki_2_fb = mapper.read_wiki_fb_mapping()
 
     print wiki_2_fb["Mount_Everest"]
