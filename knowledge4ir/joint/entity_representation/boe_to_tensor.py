f29aa131f6626285fd84aa4f30b3351199f86261
xiongchenyan <xiongchenyan@gmail.com>
2017-06-17 18:15:07 -0400

    implement boe info convert to tensor format

--- knowledge4ir/joint/entity_representation/boe_to_tensor.py
+++ knowledge4ir/joint/entity_representation/boe_to_tensor.py
@@ -14,6 +14,20 @@ import numpy as np
 import json
 import sys
 import logging
+from knowledge4ir.utils import (
+    QUERY_FIELD,
+    TARGET_TEXT_FIELDS,
+)
+
+
+def _hash_feature(h_feature, h_feature_id):
+    if not h_feature_id:
+        h_feature_id = dict([(key, p) for p, key in enumerate(h_feature.keys())])
+    l_res = [0] * len(h_feature_id)
+    for name, score in h_feature:
+        p = h_feature_id[name]
+        l_res[p] = score
+    return l_res, h_feature_id
 
 
 def load_embedding(word2vec_in):
@@ -25,5 +39,69 @@ def load_embedding(word2vec_in):
         if not p:
             continue
         if not p % 1000:
-            logging.info('loaded %d lines')
+            logging.info('loaded %d lines', p)
+        cols = line.split()
+        l_e.append(cols[0])
+        l_emb.append(cols[1:])
+    h_e_id = zip(l_e, range(len(l_e)))
+    emb_mtx = np.array(l_emb)
+    logging.info('loaded [%d] entities, emb matrix shape: %s', len(h_e_id), json.dumps(emb_mtx.shape))
+    return h_e_id, emb_mtx
+
+
+def convert_boe_info(h_info, h_e_id, h_feature_id):
+    """
+    convert one boe info
+    :param h_info: packed q or doc's att_boe info
+    :param h_feature_id: feature name -> id
+    :return: e id hashed, att feature put in one matrix,
+    """
+    h_boe_tensor = dict()
+    for field in [QUERY_FIELD] + TARGET_TEXT_FIELDS:
+        if field not in h_info['boe']:
+            continue
+        ll_feature = []
+        l_e_id = []
+        l_e = h_info['boe']
+        l_e_id = [h_e_id[e['id']] for e in l_e]
+        for e in l_e:
+            h_f = e['feature']
+            l_feature_vector, h_feature_id = _hash_feature(h_f, h_feature_id)
+            ll_feature.append(l_feature_vector)
+
+        h_boe_tensor[field] = {'boe': l_e_id, 'att_mtx': ll_feature}
+
+    return h_boe_tensor, h_feature_id
+
+
+def process(in_name, emb_name, out_name):
+    out = open(out_name, 'w')
+    h_e_id, emb_mtx = load_embedding(emb_name)
+    np.save(out_name + '.emb', emb_mtx)
+    h_feature_id = {}
+    for p, line in enumerate(open(in_name)):
+        if not p % 1000:
+            logging.info('converted [%d] lines', p)
+        h_info = json.loads(line)
+
+        h_boe_tensor, h_feature_id = convert_boe_info(h_info, h_e_id, h_feature_id)
+        print >> out, json.dumps(h_boe_tensor)
+
+    json.dump(h_e_id, open(out_name + '.e_id.json', 'w'))
+    json.dump(h_feature_id, open(out_name + '.att_feature_id.json', 'w'), indent=1)
+    logging.info('finished, res dumped to [%s]', out_name)
+    return
+
+
+if __name__ == '__main__':
+    from knowledge4ir.utils import (
+        set_basic_log,
+    )
+    set_basic_log(logging.INFO)
+    if 4 != len(sys.argv):
+        print "convert boe to tensor format"
+        print "3 para: boe info in + embedding name + out_name"
+        sys.exit(-1)
+
+    process(*sys.argv[1:])
 
