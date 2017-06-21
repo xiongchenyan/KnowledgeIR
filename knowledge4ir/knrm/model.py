e708b7985c111687c82c1292348c5ad052fa3bbc
xiongchenyan <xiongchenyan@gmail.com>
2017-06-21 15:59:45 -0400

    debug

--- knowledge4ir/knrm/model.py
+++ knowledge4ir/knrm/model.py
@@ -94,7 +94,7 @@ class KNRM(Configurable):
     def set_embedding(self, pretrained_emb):
         self.emb = np.zeros((pretrained_emb.shape[0] + 1, pretrained_emb.shape[1]))
         self.emb[1:, :] = pretrained_emb
-        self.vocab_size, self.embedding_dim = pretrained_emb.shape
+        self.vocab_size, self.embedding_dim = self.emb.shape
 
     def _init_inputs(self):
         q_input, l_field_input = self._init_boe_input()
