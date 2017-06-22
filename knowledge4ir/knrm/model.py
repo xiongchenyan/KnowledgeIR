fc83bf9005ab8ef55db3e7d4a9cf551e1fc46c76
xiongchenyan <xiongchenyan@gmail.com>
2017-06-22 16:22:51 -0400

    try dense layer

--- knowledge4ir/knrm/model.py
+++ knowledge4ir/knrm/model.py
@@ -156,8 +156,8 @@ class KNRM(Configurable):
             input_dim=len(self.l_d_field) * len(self.mu) + self.ltr_feature_dim
         )
         if self.metric_learning:
-            self.distance_metric = DiagnalMetric(input_dim=self.embedding_dim)
-                # Dense(50, input_dim=self.embedding_dim ,use_bias=False)
+            # self.distance_metric = DiagnalMetric(input_dim=self.embedding_dim)
+            self.distance_metric = Dense(50, input_dim=self.embedding_dim ,use_bias=False)
 
     def _init_ranker(self, q_input, l_field_input, ltr_input=None, aux=False):
         """
