0e707a5dd113d527914444d8cf102ba0fed02834
xiongchenyan <xiongchenyan@gmail.com>
2017-06-27 11:38:28 -0400

    cnt exact

--- knowledge4ir/knrm/analysis/trans_mtx_bins.py
+++ knowledge4ir/knrm/analysis/trans_mtx_bins.py
@@ -19,8 +19,8 @@ if len(sys.argv) < 2:
     sys.exit(-1)
 
 
-l_bin_ed = [1 - 0.2 * i for i in range(10)]
-l_bin_st = [ed - 0.2 for ed in l_bin_ed]
+l_bin_ed = [1.00001] + [1 - 0.2 * i for i in range(10)]
+l_bin_st = [0.99999] + [ed - 0.2 for ed in l_bin_ed]
 l_bin = zip(l_bin_st, l_bin_ed)
 print "bins: %s" % json.dumps(l_bin)
 
@@ -28,6 +28,7 @@ print "loading npy"
 m = np.load(sys.argv[1])
 print m.shape
 l_bin_cnt = []
+
 for st, ed in l_bin:
     cnt = np.sum((m < ed) & (m >= st))
     l_bin_cnt.append(cnt)
