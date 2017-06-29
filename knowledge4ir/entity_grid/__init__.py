8d9e2c96ac3cd0a46be14c5627df375fb7940b8b
xiongchenyan <xiongchenyan@gmail.com>
2017-06-29 14:34:07 -0400

    frequency count

---
+++ knowledge4ir/entity_grid/__init__.py
@@ -0,0 +1,6 @@
+"""
+document modeling using entity grid
+the entity grid can be used to form better BOE?
+    e.g. salience?
+maybe also can be used for classification?
+"""
--- knowledge4ir/explicit_graph/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-form entity anchored graph like representation for document?
-
-first get sentences that contains entity ana and have a look...
-"""
