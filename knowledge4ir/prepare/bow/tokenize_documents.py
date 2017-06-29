7767992e74a98cc1646c88365c00450a4c5ce2bb
xiongchenyan <xiongchenyan@gmail.com>
2017-06-29 12:31:48 -0400

    debug

--- knowledge4ir/prepare/bow/tokenize_documents.py
+++ knowledge4ir/prepare/bow/tokenize_documents.py
@@ -16,14 +16,23 @@ sys.setdefaultencoding('UTF8')
 
 def tokenize(in_name, out_name):
     out = open(out_name, 'w')
+    cnt = 0
+    err_cnt = 0
     for p, line in enumerate(open(in_name)):
         if not p % 1000:
             logging.info('tokenized [%d] lines', p)
         cols = line.split('\t')
-        cols = [' '.join(word_tokenize(col)) for col in cols]
-        print >> out, '\t'.join(cols)
+        cnt += 1
+        try:
+            cols = [cols[0]] + [' '.join(word_tokenize(col)) for col in cols[1:]]
+            print >> out, '\t'.join(cols)
+        except UnicodeDecodeError:
+            p += 1
+            print "DECODE ERROR:"
+            print line
+
     out.close()
-    logging.info('finished')
+    logging.info('finished, [%d/%d] error', err_cnt, cnt)
 
 
 if __name__ == '__main__':
