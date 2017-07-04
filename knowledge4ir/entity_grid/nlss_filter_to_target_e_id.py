dc815613fc0d10e3810ee6916528571875f1c49b
xiongchenyan <xiongchenyan@gmail.com>
2017-07-03 20:51:46 -0400

    quick filter

---
+++ knowledge4ir/entity_grid/nlss_filter_to_target_e_id.py
@@ -0,0 +1,29 @@
+"""
+filer nlss to target e id
+input:
+    nlss, first col is key
+    target id, first col is id
+output:
+    nlss with key in target id set
+"""
+
+import sys
+
+if 4 != len(sys.argv):
+    print "filter nlss"
+    print "3 para: nlss in + target e id in + out"
+    sys.exit(-1)
+
+s_e = set([line.split()[0] for line in open(sys.argv[2])])
+cnt = 0
+out = open(sys.argv[3], 'w')
+for line in open(sys.argv[1]):
+    line = line.strip()
+    key = line.split()[0]
+    if key in s_e:
+        print >> out, line
+        cnt += 1
+
+out.close()
+print "finished [%d/%d] found" % (cnt, len(s_e))
+
