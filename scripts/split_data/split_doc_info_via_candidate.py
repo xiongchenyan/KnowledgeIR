f9ad75b95e9fdf1852cfd301ea908d480097ab18
xiongchenyan <xiongchenyan@gmail.com>
2017-07-05 17:07:47 -0400

    split doc info for parallel

--- scripts/split_data/split_doc_info_via_candidate.py
+++ scripts/split_data/split_doc_info_via_candidate.py
@@ -12,22 +12,28 @@ import sys
 import json
 import logging
 import ntpath
-from knowledge4ir.utils import load_trec_ranking_with_score, load_json_info
+from knowledge4ir.utils import (
+    load_trec_ranking_with_score,
+    load_json_info,
+    set_basic_log,
+)
 
 
 def mul_load_candidate_doc(in_dir):
     l_name, l_s_doc = [], []
+    logging.info('load doc partitions')
     for dir_name, sub_dir, f_names in os.path.walk(in_dir):
         for f_name in f_names:
             l_name.append(f_name)
             l_q_rank = load_trec_ranking_with_score(os.path.join(dir_name, f_name))
-            s_doc = set(sum([ [doc for doc, score in rank]    for q, rank in l_q_rank ], []))
+            s_doc = set(sum([ [doc for doc, score in rank] for q, rank in l_q_rank ], []))
             l_s_doc.append(s_doc)
+            logging.info('[%s][%d] doc', f_name, len(s_doc))
     return l_name, l_s_doc
 
 
 def partition_doc_info(doc_info_in, l_name, l_s_doc, out_dir):
-
+    logging.info('start partioning doc info')
     h_doc_info = load_json_info(doc_info_in, 'docno')
     for name, s_doc in zip(l_name, l_s_doc):
         out_name = os.path.join(out_dir, ntpath.basename(doc_info_in)) + '.' + name
@@ -42,5 +48,13 @@ def partition_doc_info(doc_info_in, l_name, l_s_doc, out_dir):
         out.close()
     logging.info('finished')
 
+if __name__ == '__main__':
+    set_basic_log()
+    if 4 != len(sys.argv):
+        print "split doc info for given data"
+        print "3 para: doc info in, splited trec dir, out dir"
+        sys.exit(-1)
 
-
+    doc_info_in, trec_in_dir, out_dir = sys.argv[:3]
+    l_name, l_s_doc = mul_load_candidate_doc(trec_in_dir)
+    partition_doc_info(doc_info_in, l_name, l_s_doc, out_dir)
