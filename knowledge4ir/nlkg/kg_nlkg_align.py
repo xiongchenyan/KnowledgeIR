fd3a72a0da5469daba400d5a00b9272209508fa7
xiongchenyan <xiongchenyan@gmail.com>
2017-07-06 15:51:13 -0400

    init kg nlkg alignment

---
+++ knowledge4ir/nlkg/kg_nlkg_align.py
@@ -0,0 +1,10 @@
+"""
+align given kg and nlkg, see the coverage and examples
+input:
+    edges and nlss in resource
+output:
+    for each entity:
+        # of nlss edge, # of kg, # of overlapped (kg's tail in the nlss)
+        [kg edge, [nlss of this kg edge]]
+"""
+
