b491962db560ad2711906cdcbe60a858dab3489d
xiongchenyan <xiongchenyan@gmail.com>
2017-07-12 16:23:10 -0400

    debug

--- knowledge4ir/boe_exact/EntityAnchor.py
+++ knowledge4ir/boe_exact/EntityAnchor.py
@@ -326,8 +326,11 @@ class EntityAnchorFeature(BoeFeature):
                     l_sim.append(0)
                     logging.warn('[%s] not in embedding', e)
                 else:
-                    l_sim.append(max(self.resource.embedding.similarity(e_id, e), 0))
+                    score = max(self.resource.embedding.similarity(e_id, e), 0)
+                    l_sim.append(score)
+                    logging.info('[%s]-[%s] sim: [%f]', e_id, e, score)
         l_binned_score = bin_similarity(l_sim, form_bins(6))
+        logging.info('[%s] bins: %s', e_id, json.dumps(l_binned_score))
         h_feature = dict(l_binned_score)
         return h_feature
 
