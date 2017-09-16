caa06d077e11dabafdd93661b3e93e81a81e4a13
xiongchenyan <xiongchenyan@gmail.com>
2017-09-16 15:30:44 -0400

    adding mini batch

--- knowledge4ir/salience/translation_model.py
+++ knowledge4ir/salience/translation_model.py
@@ -78,3 +78,68 @@ class GraphTranslation(nn.Module):
         else:
             return output
 
+
+class BachPageRank(nn.Module):
+    """
+    input: matrix's |doc||v_e|, |doc||v_score| of these v_e
+        e=-1 is padding
+    output: p(target e id is salient)
+    """
+
+    def __init__(self, layer, vocab_size, embedding_dim, pre_embedding=None):
+        super(BachPageRank, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.linear = nn.Linear(1, 2, bias=True)
+        if use_cuda:
+            logging.info('copying parameter to cuda')
+            self.embedding.cuda()
+            self.linear.cuda()
+        self.layer = layer
+        if pre_embedding is not None:
+            self.embedding.weight.data.copy_(torch.from_numpy(pre_embedding))
+        return
+
+    def forward(self, mtx_e, mtx_score):
+        """
+        return probability of each one being salient
+        :param v_e: the input entity id's, has to be Variable()
+        :param v_score: the initial weights on each entity, has to be Variable()
+        :return: score for each one
+        """
+        mtx_embedding = self.embedding(mtx_e, padding_idx=-1)
+        mtx_embedding = mtx_embedding.div(
+            torch.norm(mtx_embedding, p=2, dim=-1, keepdim=True).expand_as(mtx_embedding))
+
+        trans_mtx = torch.matmul(mtx_embedding, mtx_embedding.transpose(-2, -1)) + 1.0
+        trans_mtx = trans_mtx.div(
+            torch.norm(trans_mtx, p=1, dim=-2, keepdim=True).expand_as(trans_mtx)
+        )
+        # mid = trans_mtx.cpu().data.numpy()
+        # if np.sum(np.isnan(mid)):
+        #     logging.info('entities:\n%s',
+        #                  json.dumps(v_e.data.cpu().numpy().tolist())
+        #                  )
+        #     logging.info('init scores:\n%s',
+        #                  json.dumps(v_score.data.cpu().numpy().tolist())
+        #                  )
+        #     logging.info('linear weights:\n%s',
+        #                  json.dumps(self.linear.weight.data.cpu().numpy().tolist()))
+        #     logging.info('embeddings are:\n %s',
+        #                  json.dumps(mtx_embedding.data.cpu().numpy().tolist())
+        #                  )
+        #     logging.info('trans_mtx:\n %s',
+        #                  json.dumps(trans_mtx.data.cpu().numpy().tolist())
+        #                  )
+        #
+        #     raise ValueError
+        output = mtx_score.unsqueeze(-1)
+        for p in xrange(self.layer):
+            output = torch.matmul(trans_mtx, output)
+
+        # output = F.log_softmax(self.linear(output))
+        output = torch.stack([F.log_softmax(output[i]) for i in range(output.size()[0])])
+        # output = output.squeeze(-1)
+        if use_cuda:
+            return output.cuda()
+        else:
+            return output
