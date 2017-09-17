6689e46d42ddb10600eb8378dd9e8c9a5af3b591
xiongchenyan <xiongchenyan@gmail.com>
2017-09-17 16:45:43 -0400

    debug

--- knowledge4ir/salience/utils.py
+++ knowledge4ir/salience/utils.py
@@ -9,7 +9,7 @@ from torch.autograd import Variable
 
 def hinge_loss(output, target):
     assert not target.requires_grad
-    loss = target.type(torch.FloatTensor) * (1.0 - output)
+    loss = torch.mm(target.type(torch.FloatTensor), (target.type(torch.FloatTensor) - output))
     loss = loss.clamp(min=0).sum()
     return loss
 
