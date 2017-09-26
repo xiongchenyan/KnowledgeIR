be9534b607b1fff2d0a077fa0866a673aad4e499
Chenyan Xiong <xiongchenyan@gmail.com>
2017-09-26 12:53:38 -0400

    implementing uw local context io

--- knowledge4ir/salience/data_io.py
+++ knowledge4ir/salience/data_io.py
@@ -6,9 +6,8 @@ import json
 import torch
 from torch.autograd import Variable
 
-from knowledge4ir.salience.center import use_cuda
 from knowledge4ir.utils import term2lm
-
+use_cuda = torch.cuda.is_available()
 
 def padding(ll, filler):
     n = max([len(l) for l in ll])
@@ -97,5 +96,87 @@ def feature_io(l_line, spot_field, in_field, salience_field, max_e_per_d=None):
         "mtx_e": m_e,
         "ts_feature": ts_feature
     }
-
     return h_packed_data, m_label
+
+
+def uw_io(l_line, spot_field, in_field, salience_field, max_e_per_d=None):
+    """
+    prepare local words around each entity's location
+    :param l_line: hashed data with loc fields
+    :param spot_field: together with in_field, to get
+    :param in_field:
+    :param salience_field: to get label
+    :param max_e_per_d:
+    :return:
+    """
+    sent_len = 10
+    max_sent_cnt = 0
+    ll_e = []
+    ll_label = []
+    lll_sent = []   #
+
+    for line in l_line:
+        h = json.loads(line)
+        packed = h[spot_field].get(in_field, {})
+        l_e = packed.get('entities', [])
+        ll_loc = packed.get('loc', [])
+        l_words = h[in_field]
+        ll_sent = [_form_local_context(l_loc, l_words, sent_len)
+                   for l_loc in ll_loc]
+
+        this_max_sent_cnt = max([len(l_sent) for l_sent in ll_sent])
+        max_sent_cnt = max(max_sent_cnt, this_max_sent_cnt)
+
+        s_salient_e = set(h[spot_field].get(salience_field, {}).get('entities', []))
+        l_label = [1 if e in s_salient_e else -1 for e in l_e]
+
+        ll_e.append(l_e)
+        ll_label.append(l_label)
+        lll_sent.append(ll_sent)
+
+    for d_p in xrange(len(lll_sent)):
+        for e_p in xrange(len(lll_sent[d_p])):
+            lll_sent[d_p][e_p] += [[0] * sent_len] * (max_sent_cnt - len(lll_sent[d_p][e_p]))
+
+    ll_e = padding(ll_e, 0)
+    ll_label = padding(ll_label, 0)
+
+    l_empty_e_sent = [[0] * sent_len] * max_sent_cnt
+    lll_sent = padding(lll_sent, l_empty_e_sent)
+
+    m_e = Variable(torch.LongTensor(ll_e)).cuda() \
+        if use_cuda else Variable(torch.LongTensor(ll_e))
+    m_label = Variable(torch.FloatTensor(ll_label)).cuda() \
+        if use_cuda else Variable(torch.FloatTensor(ll_label))
+    ts_local_context = Variable(torch.FloatTensor(lll_sent)).cuda() \
+        if use_cuda else Variable(torch.FloatTensor(lll_sent))
+    h_packed_data = {
+        "mtx_e": m_e,
+        "ts_local_context": ts_local_context
+    }
+    return h_packed_data, m_label
+
+
+def _form_local_context(l_loc, l_words, sent_len):
+    """
+    get local UW words
+    :param l_loc: positions to get UW words
+    :param l_words: content
+    :param sent_len: length of UW
+    :return:
+    """
+    l_sent = []
+    for st, ed in l_loc:
+        uw_st = max(st - sent_len, 0)
+        uw_st = min(uw_st, len(l_words) - sent_len)
+        sent = l_words[uw_st: uw_st + sent_len]
+        sent += [0] * (sent_len - len(sent))
+        l_sent.append(sent)
+    return l_sent
+
+
+
+
+
+
+
