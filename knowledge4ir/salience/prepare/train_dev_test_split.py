5de775ff3bcaef5fb07084e1ade519d39cc067de
Chenyan Xiong <xiongchenyan@gmail.com>
2017-12-11 17:48:31 -0500

    train test dev split with train docno given

--- knowledge4ir/salience/prepare/train_dev_test_split.py
+++ knowledge4ir/salience/prepare/train_dev_test_split.py
@@ -2,30 +2,32 @@
 split train dev and test via given docno
 input:
     whole json corpus with docno field
+    train docno's one per line
     dev docno's one per line
     test docno's one per line
 output:
+    train docs
     dev docs
     test docs
-    all else goes to train docs
 """
 
 import sys
 import json
 
 
-if 5 != len(sys.argv):
+if 6 != len(sys.argv):
     print "split to train test and dev via given docno in test and dev"
-    print "4 para: json corpus + dev doc no + test doc no + out pre"
+    print "5 para: json corpus + train doc no + dev doc no + test doc no + out pre"
     sys.exit(-1)
 
 
-s_dev_docno = set(open(sys.argv[2]).read().splitlines())
-s_test_docno = set(open(sys.argv[3]).read().splitlines())
+s_train_docno = set(open(sys.argv[2]).read().splitlines())
+s_dev_docno = set(open(sys.argv[3]).read().splitlines())
+s_test_docno = set(open(sys.argv[4]).read().splitlines())
 print '[%d] dev [%d] test doc nos' % (len(s_dev_docno), len(s_test_docno))
-train_out = open(sys.argv[4] + '.train', 'w')
-dev_out = open(sys.argv[4] + '.dev', 'w')
-test_out = open(sys.argv[4] + '.test', 'w')
+train_out = open(sys.argv[5] + '.train', 'w')
+dev_out = open(sys.argv[5] + '.dev', 'w')
+test_out = open(sys.argv[5] + '.test', 'w')
 
 train_cnt, dev_cnt, test_cnt = 0, 0, 0
 
@@ -38,7 +40,7 @@ for p, line in enumerate(open(sys.argv[1])):
     elif docno in s_test_docno:
         print >> test_out, line
         test_cnt += 1
-    else:
+    elif docno in s_train_docno:
         print >> train_out, line
         train_cnt += 1
 
