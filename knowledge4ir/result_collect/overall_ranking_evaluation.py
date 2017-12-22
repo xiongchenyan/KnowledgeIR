f18fe0f86e3b9088735560b0e335420cbe026c04
Chenyan Xiong <xiongchenyan@gmail.com>
2017-12-22 15:53:31 -0500

    support json format evaluation res

--- knowledge4ir/result_collect/overall_ranking_evaluation.py
+++ knowledge4ir/result_collect/overall_ranking_evaluation.py
@@ -10,6 +10,9 @@ input:
         a folder/
             eval.d01,- eval.d20
     name of each run
+
+12/22/2017
+    add load json format results
 """
 
 from traitlets import (
@@ -27,6 +30,7 @@ from knowledge4ir.result_collect import (
 )
 import logging
 import os
+import json
 
 
 class RankingPerformanceCollector(Configurable):
@@ -43,9 +47,15 @@ class RankingPerformanceCollector(Configurable):
                         help='symbols to indicate significances'
                         ).tag(config=True)
     out_dir = Unicode(help='out directory').tag(config=True)
+    res_format = Unicode('trec',
+                         help='the format of the evaluation files, trec or json'
+                         ).tag(config=True)
 
     l_target_metric = List(Unicode, default_value=['ndcg', 'err']).tag(config=True)
-    l_target_depth = List(Int, default_value=[1, 5, 10, 20]).tag(config=True)
+    l_target_depth = List(Int, default_value=[20]).tag(config=True)
+    main_metric = Unicode('ndcg').tag(config=True)
+    main_depth = Int(20).tag(config=True)
+
     eva_prefix = Unicode('eval.d')
     sig_str = Unicode('\dagger')
 
@@ -56,6 +66,12 @@ class RankingPerformanceCollector(Configurable):
         self.h_base_eval_per_q = []
         self.h_base_eval = []
         self.l_to_comp_run_p = [self.l_run_name.index(name) for name in self.l_sig_test_name]
+        self.h_res_loader = {
+            'trec': self._load_trec_eval_results,
+            'json': self._load_json_eval_results,
+        }
+        if self.res_format == 'json':
+            self.l_target_depth = [0]
 
     def _load_eva_results(self):
         self.h_base_eval_per_q, self.h_base_eval = self._load_per_run_results(self.baseline_name)
@@ -67,7 +83,10 @@ class RankingPerformanceCollector(Configurable):
         return
 
     def _load_per_run_results(self, run_name):
-        run_dir = os.path.join(self.result_dir, run_name)
+        eval_fname = os.path.join(self.result_dir, run_name)
+        return self.h_res_loader[self.res_format](eval_fname)
+
+    def _load_trec_eval_results(self, run_dir):
         h_eval_per_q = dict()
         h_eval = dict()
         for depth in self.l_target_depth:
@@ -87,8 +106,40 @@ class RankingPerformanceCollector(Configurable):
                 else:
                     logging.error('[%s] metric not implemented', metric)
                     raise NotImplementedError
-
         return h_eval_per_q, h_eval
+
+    def _load_json_eval_results(self, eval_fname):
+        per_data_eval_name = eval_fname + '.json'
+        final_eval_name = per_data_eval_name + '.eval'
+        h_eval = dict(
+            [item for item in json.load(open(final_eval_name))
+             if item[0] in self.l_target_metric]
+        )
+
+        h_eval_per_data = dict()
+        l_key_h_eval = []
+        for line in open(per_data_eval_name):
+            h = json.loads(line)
+            key = self._get_data_key(h)
+            if 'eval' not in h:
+                logging.warn('%s has no eval field', line)
+                continue
+            h_this_eval = h['eval']
+            l_key_h_eval.append(key, h_this_eval)
+        l_key_h_eval.sort(key=lambda item: item[0])
+        l_h_eval = [item[1] for item in l_key_h_eval]
+        for metric in self.l_target_metric:
+            l_score = [h_eval.get(metric, 0) for h_eval in l_h_eval]
+            h_eval_per_data[metric] = l_score
+        return h_eval_per_data, h_eval
+
+    def _get_data_key(self, h_data):
+        l_key_name = ['qid', 'docno']
+        for name in l_key_name:
+            if name in h_data:
+                return h_data[name]
+        logging.warn('%s has no key field', json.dumps(h_data))
+        return ""
 
     def csv_overall_performance_table(self):
         self._load_eva_results()
@@ -99,7 +150,7 @@ class RankingPerformanceCollector(Configurable):
         header = "\\bf{Method}"
         for eva_metric in self.l_target_metric:
             for d in self.l_target_depth:
-                metric = eva_metric.upper() + '@%2d' % d
+                metric = eva_metric.upper() + '@%2d' % d if d else ''
                 header += '& \\bf{%s}' % metric + '& &\\bf{W/T/L}'
 
         # for metric in [self.target_metric.upper() + '@%2d' % d for d in self.l_target_depth]:
@@ -130,12 +181,10 @@ class RankingPerformanceCollector(Configurable):
         wtl_str = ' & --/--/--'
         for d in self.l_target_depth:
             for eva_metric in self.l_target_metric:
-                metric = eva_metric + '%02d' % d
+                metric = eva_metric + '@%2d' % d if d else ''
                 score = h_eval[metric]
                 if run_name == self.baseline_name:
                     res_str += ' & $%.4f$ & -- ' % score
-                    # if d == 20:
-                    #     res_str += ' & --/--/--'
                     continue
                 l_base_q_score = self.h_base_eval_per_q[metric]
                 base_score = self.h_base_eval[metric]
@@ -144,15 +193,6 @@ class RankingPerformanceCollector(Configurable):
 
                 w, t, l = win_tie_loss(l_q_score, l_base_q_score)
 
-                # if rel > 0:
-                #     p_value = randomization_test(l_q_score, l_base_q_score)
-                # else:
-                #     p_value = randomization_test(l_base_q_score, l_q_score)
-
-                # if p_value <= 0.05:
-                #     score_str = '${%.4f}^%s$' % (score, self.sig_str)
-                # else:
-                #     score_str = '${%.4f}$' % score
                 sig_mark = self._calc_sig_mark(l_q_score, metric)
                 if sig_mark:
                     score_str = '${%.4f}^%s$' % (score, sig_mark)
@@ -162,8 +202,9 @@ class RankingPerformanceCollector(Configurable):
                     score_str,
                     "$ {0:+.2f}\\%  $ ".format(rel * 100),
                 ]) + '\n\n'
-                if (d == 20) & (eva_metric == 'ndcg'):
-                    wtl_str = '& %02d/%02d/%02d\n\n' % (w, t, l)
+                if eva_metric == self.main_metric:
+                    if (d != 0) & (d == self.main_depth):
+                        wtl_str = '& %02d/%02d/%02d\n\n' % (w, t, l)
 
         res_str += wtl_str
 
