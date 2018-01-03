34fc068618f87af38024194f3af643b916af809e
Chenyan Xiong <xiongchenyan@gmail.com>
2018-01-03 15:07:50 -0500

    debug

--- knowledge4ir/result_collect/analysis/base.py
+++ knowledge4ir/result_collect/analysis/base.py
@@ -1,5 +1,6 @@
 import logging
 import math
+import json
 
 
 def bin_score(l_stat, l_score, nb_bin):
@@ -8,12 +9,14 @@ def bin_score(l_stat, l_score, nb_bin):
     l_sorted_score = [item[1] for item in l_item]
     bin_width = int(math.ceil(len(l_stat) / float(nb_bin)))
     logging.info('binning [%d] data to [%d] size [%d] bin', len(l_item), nb_bin, bin_width)
+    logging.info('[%d] item: %s', len(l_item), json.dumps(l_item))
     l_bin_res = []
     st = 0
     ed = bin_width
     l_bin_range = []
     while st <= len(l_sorted_score):
         l_this_bin = l_sorted_score[st: ed]
+        logging.info('bin [%d] [%d:%d]', len(l_bin_res), st, ed)
         b_st, b_ed = l_item[st][0], l_item[min(ed, len(l_item)-1)][0]
         logging.info('bin range %f, %f', b_st, b_ed)
         l_bin_range.append((b_st, b_ed))
