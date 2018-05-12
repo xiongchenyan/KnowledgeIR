c339cd761d8afd5066c3cca98f55ef96c11aa88f
hunterhector <hunterhector@gmail.com>
2018-05-12 17:34:28 -0400

    write out as json.

--- knowledge4ir/salience/analysis/intrusion_creator.py
+++ knowledge4ir/salience/analysis/intrusion_creator.py
@@ -1,81 +1,79 @@
+from __future__ import print_function
 import json
 from collections import Counter
+import sys
 
 
 def apply_selector(events, selections):
     new_events = {
-        'bodyText': {
-            'sparse_features': {
-                'LexicalHead': [],
-                'SparseFrameName': [],
-            },
-            'salience': [0] * len(selections),
-            'features': [],
-        }
+        'sparse_features': {
+            'LexicalHead': [],
+            'SparseFrameName': [],
+        },
+        'salience': [0] * len(selections),
+        'features': [],
     }
 
     for index in selections:
-        new_events['bodyText']['sparse_features']['LexicalHead'].append(
-            events['bodyText']['sparse_features']['LexicalHead'][index]
+        new_events['sparse_features']['LexicalHead'].append(
+            events['sparse_features']['LexicalHead'][index]
         )
-        new_events['bodyText']['sparse_features']['SparseFrameName'].append(
-            events['bodyText']['sparse_features']['SparseFrameName'][index]
+        new_events['sparse_features']['SparseFrameName'].append(
+            events['sparse_features']['SparseFrameName'][index]
         )
-        new_events['bodyText']['features'].append(
-            events['bodyText']['features'][index]
+        new_events['features'].append(
+            events['features'][index]
         )
-
-    return events
+    return new_events
 
 
 def increment_merge(origin_events, intruder_events, adjuster):
     l_merged_events = []
 
-    num_intruder = len(intruder_events['salience'])
-
-    for until in range(1, num_intruder + 1):
+    for until in range(1, len(intruder_events['salience']) + 1):
         merged_events = {
-            'bodyText': {
-                'sparse_features': {
-                    'LexicalHead': [],
-                    'SparseFrameName': [],
-                },
-                'salience': [1] * len(origin_events['salience']) + [0] * until,
-                'features': [],
-            }
+            'sparse_features': {
+                'LexicalHead': [],
+                'SparseFrameName': [],
+            },
+            'salience': [1] * len(origin_events['salience']) + [0] * until,
+            'features': [],
         }
 
-        merged_events['bodyText']['sparse_features']['LexicalHead'].extend(
-            origin_events['bodyText']['sparse_features']['LexicalHead']
+        merged_events['sparse_features']['LexicalHead'].extend(
+            origin_events['sparse_features']['LexicalHead']
         )
-        merged_events['bodyText']['sparse_features']['SparseFrameName'].extend(
-            origin_events['bodyText']['sparse_features']['SparseFrameName']
+        merged_events['sparse_features']['SparseFrameName'].extend(
+            origin_events['sparse_features']['SparseFrameName']
         )
-        merged_events['bodyText']['features'].extend(
-            origin_events['bodyText']['features']
+        merged_events['features'].extend(
+            origin_events['features']
         )
 
-        merged_events['bodyText']['sparse_features']['LexicalHead'].extend(
-            intruder_events['bodyText']['sparse_features'][
+        merged_events['sparse_features']['LexicalHead'].extend(
+            intruder_events['sparse_features'][
                 'LexicalHead'][:until]
         )
-        merged_events['bodyText']['sparse_features']['SparseFrameName'].extend(
-            intruder_events['bodyText']['sparse_features'][
+        merged_events['sparse_features']['SparseFrameName'].extend(
+            intruder_events['sparse_features'][
                 'SparseFrameName'][:until]
         )
 
-        intruder_features = intruder_events['bodyText']['features'][:until]
+        intruder_features = intruder_events['features'][:until]
 
         adjusted_features = []
 
         for index, features in enumerate(intruder_features):
-            new_f = features[:]
-            new_f[-2] -= adjuster[index]
+            freq = features[-2] - adjuster[index]
+            new_f = [0] * len(features)
+            new_f[-2] = freq
             adjusted_features.append(new_f)
 
-        merged_events['bodyText']['features'].extend(
+        merged_events['features'].extend(
             adjusted_features
         )
+
+        l_merged_events.append(merged_events)
 
     return l_merged_events
 
@@ -125,54 +123,60 @@ def mix(origin_doc, intruding_doc):
 
     non_sa_events = apply_selector(intruder_events, non_salient_indices)
 
-    origin_events = origin_doc['event']['docText']
+    origin_events = origin_doc['event']['bodyText']
     sa_set = increment_merge(origin_events, sa_events, sa_adjuster)
     non_sa_set = increment_merge(origin_events, non_sa_events, non_sa_adjuster)
 
-    return add_meta(origin_doc, sa_set), add_meta(origin_doc, non_sa_set)
+    meta_sa_set = [add_meta(origin_doc, doc) for doc in sa_set]
+    meta_non_sa_set = [add_meta(origin_doc, doc) for doc in non_sa_set]
+
+    return meta_sa_set, meta_non_sa_set
 
 
 def create_intruders(hashed_corpus, output_path, num_origin, num_intruder_per):
     count = 0
+    intruder_count = 0
 
     origins = []
 
-    num_intruders = num_intruder_per * num_origin
-
     with open(hashed_corpus) as data_in, open(output_path, 'w') as data_out:
         for line in data_in:
-            doc_info = json.load(line)
-            salience_labels = doc_info['events']['bodyText']['salience']
+            doc_info = json.loads(line)
+            salience_labels = doc_info['event']['bodyText']['salience']
 
             num_salience = sum(salience_labels)
 
             if num_salience < 5:
                 continue
 
-            l_test_pack = []
-
             if count < num_origin:
                 origins.append(doc_info)
-            elif count < num_intruders + num_origin:
-                for origin in origins:
+            elif count >= num_origin and intruder_count < num_intruder_per:
+                intruder_count += 1
+                for i, origin in enumerate(origins):
                     sa_docs, non_sa_docs = mix(origin, doc_info)
-                    l_test_pack.append(
-                        {
-                            'salient_test': sa_docs,
-                            'non_salient_test': non_sa_docs,
-                        }
+                    test_pack = {
+                        'salient_test': sa_docs,
+                        'non_salient_test': non_sa_docs,
+                    }
+
+                    print(
+                        "\rProcessed %d intruders for origin %d" % (
+                            intruder_count, i + 1
+                        ), end=''
                     )
+                    data_out.write(json.dumps(test_pack) + '\n')
+
             else:
+                print('\nDone.')
                 break
 
             count += 1
 
-            data_out.write(json.dumps(l_test_pack) + '\n')
-
 
 if __name__ == '__main__':
-    import sys
-
     corpus_path, output_path, num_origin, num_intruder = sys.argv[1:5]
+    num_origin = int(num_origin)
+    num_intruder = int(num_intruder)
 
     create_intruders(corpus_path, output_path, num_origin, num_intruder)
